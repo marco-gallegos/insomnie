0eac97ccc7934001bf63ab1ed4c8bd385a2eebb9
Marco A. Gallegos <ma_galeza@hotmail.com>
2024-10-16 23:29:21 -0600

    feat(cli): added docs with vitepress and version json generation

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+// .vitepress/config.js
+export default {
+  // site-level options
+  title: 'Insomnie Documentation',
+  description: 'Light Weight Http Client.',
+
+  themeConfig: {
+    // theme-level options
+  }
+}
